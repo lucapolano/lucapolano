8c4e08daa67f63f0054ff1653062e1e57e1233b5
LUCA POLANO <>
2021-12-10 12:10:53 +0000

    main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,46 @@
+c = 200
+print(hex(c))
+print(bin(c))
+print(100/3)
+print(100//3)
+print(100.0/3)
+print(100.0//3)
+print(100%3)
+print(divmod(100,3))
+r=13+7j
+print(type(r))
+print(r.real)
+print(type(r.imag))
+print(pow(2,3,3))
+print(round(2.5434435,3))
+print(round(2.5435435,3))
+print(34%5)
 a=2
 z=3+4j
 print(a+z)
+import math
+print(math.pi)
+a=1
+print(type(a))
+if(a): print('True')
+a=False
+print(type(a))
+a="""I am a string spanning in 3 rows,
+... containing 'sigle quotes',
+... containing "double quotes",
+... containing '''triple quotes''' ... """
+print(a)
+b='''I am a string spanning in 3 rows,... containing 'sigle quotes',... containing "double quotes",... containing """triple quotes"""... '''
+print(b)
+var1 = 'Hello World!'
+var2 = "Python Programming"
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[1:5])
+print("var1[1:2]: ",var1[1:2])
+print("var1[1:3]: ",var1[1:3])
+print("var1[0]: ",var1[3])
+print(var1+var1)
+name="Peter"
+my_string="Hello %s" %name
+print(my_string)
+
