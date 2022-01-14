dad79790e3679de69a4e4523a79a7b4320a519c9
LUCA POLANO <95913833+lucapolano@users.noreply.github.com>
2022-01-14 09:17:06 +0000

    ex 2

--- src/main.py
+++ src/main.py
@@ -1,46 +1,60 @@
-c = 200
-print(hex(c))
-print(bin(c))
-print(100/3)
-print(100//3)
-print(100.0/3)
-print(100.0//3)
-print(100%3)
-print(divmod(100,3))
-r=13+7j
-print(type(r))
-print(r.real)
-print(type(r.imag))
-print(pow(2,3,3))
-print(round(2.5434435,3))
-print(round(2.5435435,3))
-print(34%5)
-a=2
-z=3+4j
-print(a+z)
-import math
-print(math.pi)
-a=1
-print(type(a))
-if(a): print('True')
-a=False
-print(type(a))
-a="""I am a string spanning in 3 rows,
-... containing 'sigle quotes',
-... containing "double quotes",
-... containing '''triple quotes''' ... """
-print(a)
-b='''I am a string spanning in 3 rows,... containing 'sigle quotes',... containing "double quotes",... containing """triple quotes"""... '''
-print(b)
-var1 = 'Hello World!'
-var2 = "Python Programming"
-print ("var1[0]: ", var1[0])
-print ("var2[1:5]: ", var2[1:5])
-print("var1[1:2]: ",var1[1:2])
-print("var1[1:3]: ",var1[1:3])
-print("var1[0]: ",var1[3])
-print(var1+var1)
-name="Peter"
-my_string="Hello %s" %name
-print(my_string)
+import time
+l=range(100000000)
+v=range(1000000)
+T1=time.time()
+s=list(l)+list(v)
+T2=time.time()
+print(' + execution time: :' , T2-T1,  's')
+T3=time.time()
+l.extend(v)
+T4=time.time()
+print('extend execution time:', T4-T3 , 's')
+stack=[1, 2, 3, 4]
+print("Initial Stack : ", stack)
+for i in range(5,7):
+  stack.append(i)
+  print ("Append: ", stack)
+  stack.pop()
+  print ("Pop: " , stack)
+queue=[ 'a','b','c','d' ]
+print("Initial Queue : ", queue)
+queue.append('e')
+queue.append('f')
+print("Append : ", queue)
+queue.pop(0)
+print("Pop : ", queue)
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5, 6, 7)
+print("tup1[0]: ", tup1[0])   
+print("tup2[1:5]: ", tup2[1:5])  
+tup = ('physics', 'chemistry', 1997, 2000)
+print(tup)
+mydict={1:'Lucrezia', 2:'Luca'}
+print(mydict)
+mydict2={'name':'John', 1:[2,3,4]}
+print(mydict2)
+my_dict = dict([(1,'apple'), (2,'ball')])
+print(my_dict)
+print(my_dict[1])
+print(my_dict.get(1))
+my_dict['yes']='no'
+print(my_dict)
+squares={x:x*x for x in range(7)}
+print(squares)
+odd_squares={x:x*x for x in range(7) if x%2==1}
+print(odd_squares)
+number = 23
+var = True
 
+
+while var:
+    guess=int(input('Enter an integer : '))
+    if (guess==number):
+        print('yeeeeee')
+        var = False
+    elif (guess<number):
+        print('è più alto')
+    else: 
+        print('è più basso')
+
+print('fatto')    
